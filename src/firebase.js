d0c7d92d04a63633796baf3a7e6f33e84fad3136
Jayden Pierre <jaydenpierre3311@gmail.com>
2025-06-16 20:15:06 -0400

    Actualización completa del proyecto

--- src/firebase.js
+++ src/firebase.js
@@ -16,13 +16,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYiCCFXsvDCLtWQdD2VEWmNOtu3wUTs60",
-  authDomain: "nexbuy-90ddf.firebaseapp.com",
-  projectId: "nexbuy-90ddf",
-  storageBucket: "nexbuy-90ddf.appspot.com",
-  messagingSenderId: "175685493071",
-  appId: "1:175685493071:web:0a03ed9f1d32908aa2a53a",
-  measurementId: "G-5W8HM1Q8D0"
+ apiKey: "AIzaSyAvJAAJdYItG34UbBJgBvERjFr4aCq0emM",
+  authDomain: "phantom-deals.firebaseapp.com",
+  projectId: "phantom-deals",
+  storageBucket: "phantom-deals.firebasestorage.app",
+  messagingSenderId: "144102481020",
+  appId: "1:144102481020:web:79a4820eeefb728050fdf9",
+  measurementId: "G-XTE7DEJ2SE"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
