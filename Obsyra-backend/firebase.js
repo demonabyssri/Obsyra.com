71d7d81502a98b09653b6767c4351a4b0c06cc55
Jayden Pierre <jaydenpierre3311@gmail.com>
2025-06-25 11:59:43 -0400

    Actualiza firebase.js con credenciales desde .env

--- Obsyra-backend/firebase.js
+++ Obsyra-backend/firebase.js
@@ -1,14 +1,7 @@
 const admin = require("firebase-admin");
-require("dotenv").config();
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    privateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
+  credential: admin.credential.cert(serviceAccount),
 });
-
-const db = admin.firestore();
-
-module.exports = { admin, db };
